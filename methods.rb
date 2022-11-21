a84855e90564a499464ea8eb3d34b37b6e866e88
Yuriko <ygabriela.arnott@gmail.com>
2022-11-21 10:11:32 -0500

    commit tree 207cbeed3c65866b230688605f2ff1f804877aa8

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
-# Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+def greet(name)
+    puts "Hello, #{name}!"
+end
+def greet_with_default(text="programmer")
+    puts "Hello, #{text}!"
+end
+def add(num1, num2)
+    return num1 + num2
+end
+def halve(x)
+    if x.is_a?(Integer)
+      x / 2  
+    else
+      nil
+    end
+  end
+  
